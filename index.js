12a6919ff800a9c151f88a9c2d7e2672d3009c7f
tamierlan <tamerlan.ermanbet@gmail.com>
2026-01-13 22:31:08 -0500

    Initial backend

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/profile", (req, res) => {
+  res.json({
+    name: "tamerlan",
+    age: 32,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
